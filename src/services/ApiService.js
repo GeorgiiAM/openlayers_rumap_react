b3f443839176a615cafba9ed7a39b145c83f8491
Georgii <eg_egor@mail.ru>
2024-04-27 21:30:27 +0300

    Updated api service.

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,18 +1,24 @@
-export async function getAreaStatisticsByCoordinates(coordinates) {
-    const url = `http://gis01.rumap.ru/4898/areaStatistics?guid=93BC6341-B35E-4B34-9DFE-26796F64BBB7&geometry=1&geojson={"features":[{"geometry":{"coordinates":[${coordinates}],"type":"Polygon"},"properties":{},"type":"Feature"}],"type":"FeatureCollection"}`;
+import {
+    GUID
+} from '../../config.js';
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            console.error('response:', response);
-            throw new Error('Request failed');
-        }
-
-        const data = await response.json();
-        console.log('data = ', data);
-        return data;
-    } catch (error) {
-        console.error('Error:', error);
-        return null;
+export const getAreaStatisticsByCoordinates = async (coordinates) => {
+    const geojson = {
+        "features": [{
+            "geometry": {
+                "coordinates": [coordinates],
+                "type": "Polygon"
+            },
+            "properties": {},
+            "type": "Feature"
+        }],
+        "type": "FeatureCollection"
     }
+
+    const url = `http://gis01.rumap.ru/4898/areaStatistics?guid=${GUID}&geometry=1&geojson=${JSON.stringify(geojson)}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
 }
