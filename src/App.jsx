360f279bc19f5a4c6fd2b90e767244acca8cff38
Georgii <eg_egor@mail.ru>
2024-04-27 22:06:43 +0300

    Updated project to deploy.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ function App() {
       path: '*',
       element: <NotFound />,
     },
-  ]);
+  ],
+    {
+      basename: "/TestTaskValantis/",
+    }
+  );
 
   return (
     <RouterProvider router={router} />
